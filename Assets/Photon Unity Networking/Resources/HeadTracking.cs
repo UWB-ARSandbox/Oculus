72225bd4ac5f26ec10e899a94bc9348893c77d3f
TaranChristensen <TaranChristensen@gmail.com>
2017-04-03 20:25:09 -0700

    Initial Push

---
+++ Assets/Photon Unity Networking/Resources/HeadTracking.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeadTracking : MonoBehaviour
+{
+
+    public bool IsHololens;
+
+    private GameObject _camera;
+
+    // Use this for initialization
+    void Start()
+    {
+        _camera = GameObject.FindGameObjectWithTag("MainCamera");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_camera != null && IsHololens)
+        {
+            this.transform.position = _camera.transform.position;
+            this.transform.rotation = _camera.transform.rotation;
+        }
+    }
+
+}
---
+++ Assets/Photon Unity Networking/Resources/OculusHeadTracking.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OculusHeadTracking : MonoBehaviour
+{
+    private GameObject _camera;
+
+    void Start()
+    {
+        _camera = GameObject.Find("MainCamera");
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (_camera != null) {
+            this.transform.position = _camera.transform.position;
+            this.transform.rotation = _camera.transform.rotation;
+        }
+    }
+}
